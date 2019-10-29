9802d66c03aaf0a23aaf08f4edf5db316d149fb7
twalk2 <2twalk2@gmail.com>
2019-10-29 16:02:55 -0600

    updated resort file name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles/App.scss";
 import Home from "./Home";
 import Nav from "./Nav";
-import Resort from "./Resort";
+import Resort from "./resort";
 
 import {
   faArrowCircleRight,
